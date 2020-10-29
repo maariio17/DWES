e4d5afffe66198fca8f566c93485b485d75f193a
maariio17 <73340506+maariio17@users.noreply.github.com>
2020-10-29 20:18:39 +0100

    Update ejercicio3.php

--- Tema2/Relacion4/ejercicio3.php
+++ Tema2/Relacion4/ejercicio3.php
@@ -1,5 +1,5 @@
 <?php
-echo "Bienvenido";
+echo "Bienvenido/a";
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
